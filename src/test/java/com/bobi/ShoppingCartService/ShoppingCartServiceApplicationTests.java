004fbbf225ae42b25bec8cfbc5afbfa830eb1d62
holowniabartosz <holownia.bartosz@gmail.com>
2024-05-04 00:14:59 +0200

    v0

---
+++ src/test/java/com/bobi/ShoppingCartService/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobi.ShoppingCartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
